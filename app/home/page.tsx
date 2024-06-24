748e1df789f8794729aa284c0396a777fbf4aa09
ashibeko <ashibeko@gmail.com>
2024-06-24 23:13:41 +0300

    remove EmojiSection

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,7 +4,6 @@ import { siteConfig } from "@lambo/config/site";
 import { title, subtitle } from "@lambo/components/primitives";
 
 import Layout from "./layout";
-import EmojiSection from "./components/EmojiSection";
 
 export default function Home() {
   return (
